ea551d5519714e05a0e5fb83cce626dcea12ec4e
Awsthyn <aguswagner008@gmail.com>
2022-02-13 16:08:13 -0300

    added files

--- 5_post_roary/hypothetical.py
+++ 5_post_roary/hypothetical.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from Bio import SeqIO
-
+'''
 df = pd.read_csv("gene_presence_absence.csv",low_memory=False)
 
 hypotheticals = df[(df["Annotation"] == "hypothetical protein")]
@@ -12,6 +12,22 @@ for index, row in hypotheticals.iterrows():
     hash_table[row['Gene']] = True
 
 
+for seq_record in SeqIO.parse("pan_genome_reference.fa", "fasta"):
+     gene = seq_record.description.split()[1]
+     if(gene in hash_table):
+          hash_table[gene] = str(seq_record.seq)
+
+with open('hypothetical_genes.csv', 'w') as f:
+    for key in hash_table.keys():
+        f.write("%s,%s\n"%(key,hash_table[key]))
+'''
+
+df = pd.read_excel('HypotheticalFinalNONContaminated.xlsx', header=None)
+# Data structure to use an O(n) algorithm (and avoid a O(n^2) algorithm).
+hash_table = {}
+
+for index, row in df.iterrows():
+    hash_table[row[0]] = True
 for seq_record in SeqIO.parse("pan_genome_reference.fa", "fasta"):
      gene = seq_record.description.split()[1]
      if(gene in hash_table):
